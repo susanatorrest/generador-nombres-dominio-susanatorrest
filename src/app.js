de4f837029213af23b5b9ca7c2352f675aff68ee
Susana Torres Trujillo <149514280+susanatorrest@users.noreply.github.com>
2024-01-09 10:12:44 +0000

    domainsfinal

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function domains() {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let pronounIndx = 0; pronounIndx < pronoun.length; pronounIndx++) {
+      for (let adjIndx = 0; adjIndx < adj.length; adjIndx++) {
+        for (let nounIndx = 0; nounIndx < noun.length; nounIndx++) {
+          console.log(
+            `${pronoun[pronounIndx]}${adj[adjIndx]}${noun[nounIndx]}.com`
+          );
+        }
+      }
+    }
+  }
+  console.log(domains());
 };
