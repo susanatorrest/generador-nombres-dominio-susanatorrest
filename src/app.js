8a52a9834709a37de1282de9cab53439a9821744
Susana Torres Trujillo <149514280+susanatorrest@users.noreply.github.com>
2024-02-14 03:27:43 +0000

    generador-dominios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -12,15 +11,19 @@ window.onload = function() {
     let adj = ["great", "big"];
     let noun = ["jogger", "racoon"];
 
+    let domainContainer = document.getElementById("domain-container");
+
     for (let pronounIndx = 0; pronounIndx < pronoun.length; pronounIndx++) {
       for (let adjIndx = 0; adjIndx < adj.length; adjIndx++) {
         for (let nounIndx = 0; nounIndx < noun.length; nounIndx++) {
-          console.log(
-            `${pronoun[pronounIndx]}${adj[adjIndx]}${noun[nounIndx]}.com`
-          );
+          let domainElement = document.createElement("p");
+          domainElement.textContent = `${pronoun[pronounIndx]}${adj[adjIndx]}${noun[nounIndx]}.com`;
+
+          domainContainer.appendChild(domainElement);
         }
       }
     }
   }
-  console.log(domains());
+
+  domains();
 };
